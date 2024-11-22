c17222ae4fc94b44e95204b5673f859878e38360
Allan Douglas <allandouglascode@gmail.com>
2024-11-21 23:36:49 -0300

    form margin adjusted

--- src/components/secao_formulario.jsx
+++ src/components/secao_formulario.jsx
@@ -6,7 +6,7 @@ export default function SecaoFormulario() {
         <>  
             <div className="flex flex-col h-[40rem] flex justify-center items-center content-center relative bg-white w-full my-20">
                 {/* <img src="/public/freelance-work-7308505_1920.png" className='absolute opacity-9' alt="" />  */}
-                <div className="flex flex-col justify-center items-center">
+                <div className="flex flex-col justify-center items-center mx-1">
                     <h1 className="text-2xl font-semibold md:text-3xl md:font-normal">Como podemos ajudar você?</h1>
                     <h2 className="text-sm mt-3">Entre em contato conosco. Adorariamos ouvir você.</h2>
                 </div>

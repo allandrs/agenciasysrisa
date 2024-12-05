36b8c9e831a010ed39ea442dccb803cc9bb6533e
Allan Douglas <allandouglascode@gmail.com>
2024-12-05 06:45:13 -0300

    carousel v2 fixing

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,7 +8,7 @@ import Carousel from "./carousel"
 export default function Projects() {
     return (
         <>
-            <div id="portfolio" className="ctner h-[500px] md:h-[450px] bg-[#0b112b]">
+            <div id="portfolio" className="ctner h-[450px] bg-[#0b112b]">
                 <div className="flex flex-col items-center pt-16 mx-2 md:mx-4 ">
                     <h1 className="text-3xl font-extrabold text-white text-center">
                     Alguns de <span className="text-[#06d2dd]"> nossos </span>projetos
